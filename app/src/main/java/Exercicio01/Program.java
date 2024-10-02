b26af8f156cc534c44337a92f74cea6035c11a2e
Bruno Ibiapina <brunoibiapina@me.com>
2024-10-02 01:50:20 -0300

    Correção

--- app/src/main/java/Exercicio01/Program.java
+++ app/src/main/java/Exercicio01/Program.java
@@ -1,11 +1,8 @@
 package Exercicio01;
 
-import java.util.Scanner;
-
 public class Program {
     public static void main(String[] args) {
 
-        Scanner sc = new Scanner(System.in);
         Calcular calc1 = new Calcular();
 
         System.out.println(calc1.soma(4, 5));
